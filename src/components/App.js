2412074eea8c3664369ba268b8312c69e603dfbf
Clay Mangum <clay@trinitasaccounting.com>
2024-02-23 21:24:58 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+    const [dogBreeds, setDogBreeds] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(data => {
+                setDogBreeds(data.message)
+                setIsLoading(true)
+            })
+    }, []);
+
+
+
+
+    return (
+
+        <div>
+            {!isLoading ? <p>Loading...</p> : <img src={dogBreeds} alt="A Random Dog" />}
+        </div>
+    )
+}
+
+export default App;
